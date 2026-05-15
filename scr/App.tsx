02b0b23ef9bf6d26b2f098bf2ff9b834581c83f1
khalifa73-hub <dgfvztr6yv@privaterelay.appleid.com>
2026-05-15 16:47:35 -0700

    Create App component with styling and message

---
+++ scr/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ 
+      backgroundColor: '#000000', 
+      color: '#ffffff', 
+      height: '100vh', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      fontFamily: 'sans-serif',
+      fontSize: '24px',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      padding: '20px'
+    }}>
+      D1 Athlete App – If you see this on your iPhone, it works!
+    </div>
+  )
+}
+
+export default App
