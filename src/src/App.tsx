eba7515cd0f8562fe292b5581626e20e9c5b089b
khalifa73-hub <dgfvztr6yv@privaterelay.appleid.com>
2026-05-15 16:14:04 -0700

    Create App component for D1 Athlete App

---
+++ src/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      fontFamily: 'sans-serif',
+      fontSize: '1.5rem',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      padding: '20px',
+      backgroundColor: '#111',
+      color: '#fff'
+    }}>
+      D1 Athlete App – If you see this on your iPhone, it works!
+    </div>
+  );
+}
+
+export default App;
