4f0441b8349052ad872bf203a3e220b7c08064c4
khalifa73-hub <dgfvztr6yv@privaterelay.appleid.com>
2026-05-15 21:41:07 -0700

    Create Toaster component for popup alerts

---
+++ src/components/ui/toaster.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function Toaster() {
+  return (
+    <div id="toast-container" style={{ position: 'fixed', bottom: 0, right: 0, zIndex: 100 }}>
+      {/* This handles popup alerts later */}
+    </div>
+  )
+}
